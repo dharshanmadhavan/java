aa71682b847f387819b58c994ae4d4d560fb7def
dharshanmadhavan <126588290+dharshanmadhavan@users.noreply.github.com>
2024-11-30 08:45:57 +0530

    program1.java
    program1.java

---
+++ program1.java
@@ -0,0 +1,13 @@
+class program1{
+    public static void main(String[] args) {
+        int phy=55;
+        int chem=43;
+        int maths=48;
+        int opt=62;
+         
+        int avg=(phy+chem+maths+opt)/4;
+        int percentage=(100*avg)/70;
+
+        System.out.println(percentage);
+    }
+}
